95e0001716481701738b33a58dbb0716036632f3
Laksh Jadhwani <lakshjadhwani.lj@gmail.com>
2024-06-06 10:56:02 -0700

    Made use of Pointers with Receiver Functions

--- bill.go
+++ bill.go
@@ -12,25 +12,39 @@ type Bill struct {
 func GenerateBill(customerName string) Bill {
 	newBill := Bill{
 		CustomerName: customerName,
-		Items:        map[string]float64{"Pie": 5.99, "Cake": 3.99},
+		Items:        map[string]float64{},
 		Tip:          0,
 	}
 	return newBill
 }
 
 // Format the bill
-func (billInstance Bill) FormatBill() string {
-    billString := "Bill Breakdown: \n"
-    var totalAmount float64
+func (billInstance *Bill) FormatBill() string {
+	billString := "Bill Breakdown: \n"
+	var totalAmount float64
 
-    // list items
-    for itemName, itemPrice := range billInstance.Items {
-        billString += fmt.Sprintf("%-25v ...$%v \n", itemName+":", itemPrice)
-        totalAmount += itemPrice
-    }
+	// list items
+	for itemName, itemPrice := range billInstance.Items {
+		billString += fmt.Sprintf("%-25v ...$%v \n", itemName+":", itemPrice)
+		totalAmount += itemPrice
+	}
 
-    // Total
-    billString += fmt.Sprintf("%-25v ...$%0.2f", "total:", totalAmount)
+	// Tip
+	totalAmount += billInstance.Tip
+	billString += fmt.Sprintf("%-25v ...$%0.2f \n", "Tip:", billInstance.Tip)
 
-    return billString
+	// Total
+	billString += fmt.Sprintf("%-25v ...$%0.2f", "Total:", totalAmount)
+
+	return billString
+}
+
+// Update tip
+func (billInstance *Bill) UpdateTip(tipAmount float64) {
+	billInstance.Tip = tipAmount
+}
+
+// Add items to the bill
+func (billInstance *Bill) AddItem(itemName string, itemPrice float64) {
+	billInstance.Items[itemName] = itemPrice
 }
--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import "fmt"
 
 func main() {
 	customerBill := GenerateBill("Mario's Bill")
-	
+
+	customerBill.AddItem("Onion Soup", 4.5)
+	customerBill.AddItem("Veg Pie", 8.95)
+	customerBill.AddItem("Toffee Pudding", 4.95)
+	customerBill.AddItem("Coffee", 3.55)
+
+	customerBill.UpdateTip(10)
+
 	fmt.Println(customerBill.FormatBill())
 }
